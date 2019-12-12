5e81be63ece3ba1ccd9f0cae8d67da1cc7ec9bba
Ricky Burgin <ricky@burg.in>
2019-12-12 16:32:20 +0900

    minor improvement to styling

---
+++ _site/check.js
@@ -0,0 +1,22 @@
+const puppeteer = require('puppeteer');
+
+(async () => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto('https://www.apple.com/watchos/feature-availability');
+
+    const present = await page.evaluate(() => {
+        var elems = Array.from(document.getElementById('branded-ecg').getElementsByTagName('li'));
+        for (let i = 0; i < elems.length; i++) {
+            console.log(elems[i].innerText);
+            if (elems[i].innerText == "Japan") {
+                return "YES";
+            }
+        }
+        return "NO";
+    });
+
+    console.log(present);
+
+    await browser.close();
+})();
