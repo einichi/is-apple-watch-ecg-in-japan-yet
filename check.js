3fc984eed277293cb1f377d572ad4713849ef4a5
Ricky Burgin <ricky@burg.in>
2019-12-12 13:51:52 +0900

    initial site creation, automation with jekyll and github actions

--- check.js
+++ check.js
@@ -5,19 +5,18 @@ const puppeteer = require('puppeteer');
     const page = await browser.newPage();
     await page.goto('https://www.apple.com/watchos/feature-availability');
 
-    // Get the "viewport" of the page, as reported by the page.
     const present = await page.evaluate(() => {
         var elems = Array.from(document.getElementById('branded-ecg').getElementsByTagName('li'));
         for (let i = 0; i < elems.length; i++) {
             console.log(elems[i].innerText);
             if (elems[i].innerText == "Japan") {
-                return true;
+                return "YES";
             }
         }
-        return false;
+        return "NO";
     });
 
-    console.log(present);
+    console.log("present: ", present);
 
     await browser.close();
 })();
